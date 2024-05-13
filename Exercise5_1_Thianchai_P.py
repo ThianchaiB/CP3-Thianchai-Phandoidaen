15840fc7085cfcc0ce62031dc7c9d3b03b207224
ThianchaiB <thiantai_phandoidaen@cmu.ac.th>
2024-05-13 21:36:00 +0700

    Exercise 5
    type 1

---
+++ Exercise5_1_Thianchai_P.py
@@ -0,0 +1,7 @@
+# กำหนดให้ลงใส่ค่าลงไป
+x = int(input("number 1 :"))
+y = int(input("number 2 :"))
+print(x, "+", y, "=", x+y)
+print(x, "-", y, "=", x-y)
+print(x, "*", y, "=", x*y)
+print(x, "/", y, "=", x/y)
