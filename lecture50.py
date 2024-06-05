c0b61eb10e8e78521a0fe03b6574776745e8764a
ThianchaiB <thiantai_phandoidaen@cmu.ac.th>
2024-06-05 08:28:35 +0700

    Add files via upload

---
+++ lecture50.py
@@ -0,0 +1,14 @@
+def addnumber(x,y):
+    print(x+y)
+    print(x*y)
+    print(x/y)
+def sumnumber(x,y):
+    print(x+y)
+def multiplynumber(x,y):
+    print(x*y)
+def dividenumber(x,y):
+    print(x/y)
+addnumber(5,10)
+sumnumber(6,7)
+multiplynumber(8,10)
+dividenumber(7,2)
